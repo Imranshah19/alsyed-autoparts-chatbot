fe807c1c214de284d9616e3967124e4c6d4b7405
Syed Imran Shah <159697301+Imranshah19@users.noreply.github.com>
2026-03-26 18:18:52 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ STYLE:
 • Sirf auto parts topic pe raho, short jawab do`;
 };
 
-const CATS = ["Toyota","Honda","Suzuki","Kia/Hyundai","Mazda T3500","Motorcycle","Engine Oil","Other"];
+const CATS = ["Toyota","Honda","Suzuki","Suzuki 2 Stock","Kia/Hyundai","Mazda T3500","Mazda T4100","Shezore","Master","Chamber","Motorcycle","Engine Oil","Other"];
 
 function Dots() {
   return (
