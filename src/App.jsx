9e3516e5ba461eaac6d17d449b85c4148cb0d4ba
Syed Imran Shah <159697301+Imranshah19@users.noreply.github.com>
2026-03-26 17:24:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://tholqekexlpgcbkaemft.supabase.co";
 const SUPABASE_KEY = "sb_publishable_UQIulgzaOuBggjQYg6AlJA_-2_oN4zG";
